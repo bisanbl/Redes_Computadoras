654938fd5c783160fa1de9a2080525f096b237f6
Bismarck <bisanbl94@gmail.com>
2019-10-07 11:22:48 -0600

    Agregada Prueba del 3 octubre

---
+++ EntregaPrueba/s2/root/vlan-s2.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+# Desactivar switch s2
+ifconfig s2 down 2>/dev/null
+
+# Borrar switch s2
+brctl delbr s2 2>/dev/null
+
+##########################################
+# Crear configuración para VLAN número 10
+##########################################
+
+# 1. Crear interfaz/ces con etiqueta VLAN 10
+vconfig add eth3 10
+vconfig add eth4 10
+
+# 2. Activar la/s interfaz/ces con etiqueta VLAN creadas en el paso anterior
+ifconfig eth3.10 up
+ifconfig eth4.10 up
+
+# 3. Crear switch con nombre vs10
+brctl addbr vs10
+
+# 4. Añadir las interfaces necesarias al switch
+brctl addif vs10 eth3.10
+brctl addif vs10 eth4.10
+brctl addif vs10 eth0
+
+# 5. Activar el switch
+ifconfig vs10 up
+
+##########################################
+# Crear configuración para VLAN número 11
+##########################################
+
+# 1. Crear interfaz/ces con etiqueta VLAN 11
+vconfig add eth3 11
+vconfig add eth4 11
+
+# 2. Activar la/s interfaz/ces con etiqueta VLAN creadas en el paso anterior
+ifconfig eth3.11 up
+ifconfig eth4.11 up
+
+# 3. Crear switch con nombre vs11
+brctl addbr vs11
+
+# 4. Añadir las interfaces necesarias al switch
+brctl addif vs11 eth3.11
+brctl addif vs11 eth4.11
+brctl addif vs11 eth1
+
+# 5. Activar el switch
+ifconfig vs11 up
+
+##########################################
+# Crear configuración para VLAN número 12
+##########################################
+
+# 1. Crear interfaz/ces con etiqueta VLAN 12
+vconfig add eth3 12
+vconfig add eth4 12
+
+# 2. Activar la/s interfaz/ces con etiqueta VLAN creadas en el paso anterior
+ifconfig eth3.12 up
+ifconfig eth4.12 up
+
+# 3. Crear switch con nombre vs12
+brctl addbr vs12
+
+# 4. Añadir las interfaces necesarias al switch
+brctl addif vs12 eth3.12
+brctl addif vs12 eth4.12
+brctl addif vs12 eth2
+
+# 5. Activar el switch
+ifconfig vs12 up
